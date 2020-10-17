527a37e78061f181909d1082673adcb986fb17c2
Jacky <40329372+jackykwe@users.noreply.github.com>
2020-10-17 21:47:19 +0100

    Added Splash Screen (the greeting screen when starting the app)

--- Frontend/app/src/main/java/com/upick/upick/activities/MainActivity.kt
+++ Frontend/app/src/main/java/com/upick/upick/activities/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.upick.upick.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -23,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setTheme(R.style.AppTheme_NoActionBar)  // To remove splash
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
